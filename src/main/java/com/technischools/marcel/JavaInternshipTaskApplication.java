6a8e9a9c661d49ab7177ed37dbf84c586450c3d5
Marcel Geba <gibbon.master@gmail.com>
2025-02-27 14:21:26 +0100

    test app

---
+++ src/main/java/com/technischools/marcel/JavaInternshipTaskApplication.java
@@ -0,0 +1,12 @@
+package com.technischools.marcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JavaInternshipTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInternshipTaskApplication.class, args);
+    }
+}
